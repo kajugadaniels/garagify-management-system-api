cfab897b2fb1580f61b11285fc63cb0f31a9a7e3
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-26 23:25:14 +0200

    Add base app

---
+++ base/urls.py
@@ -0,0 +1,13 @@
+from base.views import *
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'base'
+
+urlpatterns = [
+    #
+] 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
