af675c1604d4b736574e8c4ab1b7c020ac8acbf1
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-26 23:39:53 +0200

    Migrations

--- base/models.py
+++ base/models.py
@@ -9,12 +9,13 @@ class Vehicle(models.Model):
     )
     model = models.CharField(max_length=255, null=True, blank=True)
     year = models.CharField(max_length=255, null=True, blank=True)
-    color = models.PositiveIntegerField(max_length=255, null=True, blank=True)
+    color = models.PositiveIntegerField(null=True, blank=True)
     plate_number = models.PositiveIntegerField(unique=True)
     created_at = models.DateField(default=timezone.now)
 
     class Meta:
         ordering = ['-created_at']
+        verbose_name_plural = "Vehicles"
 
     def __str__(self):
         return f"Vehicle({self.model}) by ({self.customer.name}) on {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
