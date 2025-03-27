f9e66bb29e97b1a5c81c2ad3e0e3c7ecbc3c8584
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-27 22:50:54 +0200

    New changes

--- base/serializers.py
+++ base/serializers.py
@@ -1,11 +1,14 @@
 from base.models import *
 from account.models import *
+from account.serializers import *
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 class InventorySerializer(serializers.ModelSerializer):
+    created_by = UserSerializer()
+
     class Meta:
         model = Inventory
-        fields = ('item_name', 'item_type', 'quantity', 'unit_price')
+        fields = ('item_name', 'item_type', 'quantity', 'unit_price', 'created_by')
