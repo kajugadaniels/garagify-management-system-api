fe5a8f5df6c327ba6dcd644c2ee972c74fe5e75c
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-30 14:16:00 +0200

    Created VehicleSerializer to serialize vehicle data

--- base/serializers.py
+++ base/serializers.py
@@ -14,6 +14,14 @@ class CustomerSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'name', 'username', 'email', 'phone_number', 'image', 'role')
 
+class VehicleSerializer(serializers.ModelSerializer):
+    customer = UserSerializer(read_only=True)
+    customer_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = Vehicle
+        fields = ('id', 'customer', 'customer_id', 'make', 'model', 'year', 'color', 'license_plate', 'vin', 'created_at', 'updated_at')
+
 class InventorySerializer(serializers.ModelSerializer):
     created_by = UserSerializer()
 
