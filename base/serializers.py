92970592bcbee8a3fef2771bd22d936d1ad9ff23
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-30 12:35:41 +0200

    Add CustomerSerializer

--- base/serializers.py
+++ base/serializers.py
@@ -4,8 +4,12 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-
 class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'name', 'username', 'email', 'phone_number', 'image', 'role')
+
+class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'name', 'username', 'email', 'phone_number', 'image', 'role')
