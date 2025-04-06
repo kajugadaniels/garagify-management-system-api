725b4947a6487972aa0732343ef0a2699b1a60f6
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-06 11:03:29 +0200

    Add Actions column with edit/delete links to VehicleAdmin

--- base/admin.py
+++ base/admin.py
@@ -1,12 +1,21 @@
 from base.models import *
+from django.urls import reverse
 from django.contrib import admin
+from django.utils.html import format_html
 
 @admin.register(Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
-    list_display = ('customer', 'make', 'model', 'year', 'license_plate', 'vin', 'created_at')
+    list_display = ('customer', 'make', 'model', 'year', 'license_plate', 'vin', 'created_at', 'view_actions')
     search_fields = ('make', 'model', 'license_plate', 'vin')
     list_filter = ('make', 'year')
     ordering = ('-created_at',)
+
+    def view_actions(self, obj):
+        edit_url = reverse('admin:base_vehicle_change', args=[obj.pk])
+        delete_url = reverse('admin:base_vehicle_delete', args=[obj.pk])
+        return format_html(f'<a class="button" href="{edit_url}">Edit</a> <a class="button" style="color:red;" href="{delete_url}">Delete</a>')
+    view_actions.short_description = 'Actions'
+    view_actions.allow_tags = True
 
 @admin.register(VehicleIssue)
 class VehicleIssueAdmin(admin.ModelAdmin):
