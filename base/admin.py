bfdc02372152db308a8c5979cc331190ce9b58a6
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-06 10:57:41 +0200

    Register Vehicle model in admin with list display, search, and filters

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
+from base.models import *
 from django.contrib import admin
 
-# Register your models here.
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'make', 'model', 'year', 'license_plate', 'vin', 'created_at')
+    search_fields = ('make', 'model', 'license_plate', 'vin')
+    list_filter = ('make', 'year')
+    ordering = ('-created_at',)
